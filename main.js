4d88328195017a8b9a75c3e161bf3f5e25ebdfb5
FrunzeAlexey <frunze111alex@gmail.com>
2021-07-02 15:04:32 +0300

    2d day

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function mainParse(n){
     let link = window.location
     let results = new Object()
-    for (let i = 0; i < n; i++){
+    for (let i = 1; i <= n; i++){
         let data = getData(link + "&page=" + i.toString())
+        // console.log(i.toString())
+        console.log(data)
+        // console.log(typeof data)
         results = parse(data, results)
     }
     return results
@@ -13,27 +16,37 @@ function mainParse(n){
 
 function getData(link){
     //Получение страниц
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", params, true)
+    var xhr = new XMLHttpRequest()
+    var parser = new DOMParser()
+    // console.log(link)
+    xhr.open("GET", link, true)
     xhr.send();
     xhr.onreadystatechange = function (){
+        if (xhr.readyState != 4) return;
         var x = xhr.responseText
-        console.log(x)
+        // console.log(x)
+        var t = parser.parseFromString(x.t, "text/html")
+        // console.log(t)
+        return 2;
     }
-    return x;
+    console.log(xhr.onreadystatechange)
+    return 1
 }
 
 
 function parse(data, res){
     //Получение данных со страницы
-    let t = data.getElementsByClassName("list-group-item")
+    if (data){
+        let t = data.getElementsByClassName("list-group-item")
+    }
+    else{
+        return;
+    }
+    // console.log(data)
+    // console.log(t)
     for (let i = 0; i < t.length; i++){
         console.log(t[i])
     }
-}
-
-function saveData(){
-//    Запись данных в файл/массив
 }
 
 function showData(){
