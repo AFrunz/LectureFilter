2bdc4c3f0a2a572d2042ee965b1b3f84fdcb1f2c
FrunzeAlexey <frunze111alex@gmail.com>
2021-07-09 15:44:56 +0300

    4d day. Настраиваю селекты и кнопки, небольшие доработки

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function mainParse(n){
     for (let i = 1; i <= n; i++){
         let data = getData(link + "&page=" + i.toString())
         // console.log(i.toString())
-        console.log(data)
+        // console.log(data)
         // console.log(typeof data)
         results = parse(data, results)
     }
@@ -80,10 +80,34 @@ function parse(data){
        pushItem(t[i])
         // console.log(t[i])
     }
+    showChoiceType()
 }
 
 function showData(){
+    let items = document.getElementsByClassName("list-group-item")
+    while (items.length != 0){
+        for (let i = 0; i < items.length; i++){
+            items[i].replaceWith()
+        }
+        items = document.getElementsByClassName("list-group-item")
+    }
 }
+
+
+
+function showChoiceType(){
+    let l_type = document.getElementById("l_type")
+    let type = "";
+    for (let i = 1; i < res.length; i++){
+        if (l_type.innerHTML.indexOf(`>${res[i].type}</option>`) == -1){
+            l_type.innerHTML = l_type.innerHTML + `<option value="${i}">${res[i].type}</option>\n`
+        }
+    }
+    console.log(type)
+    return type
+}
+
+
 
 //#-id
 //.-class-name
@@ -91,31 +115,57 @@ function showData(){
 
 function showFilter(){
     let node = document.getElementsByClassName("pagination")[0]
-    let strs = "<div class=\"container-fluid\">\n" +
-        "        <label for=\"l_name\"></label>\n" +
-        "        <select name=\"l_name\" id=\"l_name\" class=\"select-css\">\n" +
-        "            <option value=\"\">Выберите название</option>\n" +
-        "        </select>\n" +
-        "        <label for=\"l_type\"></label>\n" +
-        "        <select name=\"l_type\" id=\"l_type\" class=\"select-css\">\n" +
-        "            <option value=\"\">Выберите тип</option>\n" +
-        "        </select>\n" +
-        "    <div class=\"text-center\">\n" +
-        "        <button id=\"b_start\" type=\"button\" class=\"btn btn-outline-secondary\">Start</button>\n" +
-        "    </div>\n" +
+    let strs =  "<div class=\"container-fluid\">\n" +
+        "    <label for=\"l_type\"></label>\n" +
+        "    <select name=\"l_type\" id=\"l_type\" class=\"select-css\">\n" +
+        "        <option value=\"\">Выберите тип</option>\n" +
+        "        <option value=\"0\">Остальное</option>\n" +
+        "    </select>\n" +
+        "    <label for=\"l_name\"></label>\n" +
+        "    <select name=\"l_name\" id=\"l_name\" class=\"select-css\">\n" +
+        "        <option value=\"\">Выберите название</option>\n" +
+        "        <option value=\"1\"></option>\n" +
+        "    </select>\n" +
+        "    <button id=\"b_start\" type=\"button\" class=\"btn btn-outline-secondary\">Start</button>\n" +
         "</div>"
     node.insertAdjacentHTML("afterend", strs)
+    node.replaceWith()
 }
+
+
+function showChoiceName(){
+    let type = document.getElementById("l_type")
+    const num = type.selectedIndex
+
+
+
+
+    for (let i in type){
+        console.log(`${i}: ${type[i]}`)
+    }
+    console.log(type)
+}
+
+
+
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     var link = document.getElementById('b_start');
 //     // onClick's logic below:
-//     link.addEventListener('click', showFilter())
+//     link.addEventListener('click', showData())
 // });
 
 
-
-
+//Получение данных
 mainParse(2)
-console.log(res);
 showFilter()
+document.querySelector("#b_start").onclick = function(){
+    showData()
+}
+document.querySelector("#l_type").onchange = function(){
+    showChoiceName()
+}
+
+
+//Отображение фильтра
+// Нажатие на кнопку поиска
